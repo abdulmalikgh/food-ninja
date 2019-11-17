d17b72cb513b79948dcd8c99ae4acbcdc9724b47
abdulmalikgh <50835233+abdulmalikgh@users.noreply.github.com>
2019-11-16 23:32:23 -0800

    added ofline page

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
 const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1';
+
 const assets = [
     '/',
     '/index.html',
@@ -9,9 +11,19 @@ const assets = [
     '/css/bootstrap.min.css',
     '/css/fontawesome.min.css',
     '/css/index.css',
-    'images/doctor.jpg'
+    '/images/dish.png',
+    '/pages/fallback.html'
 ] 
 
+const limitCacheSize = (name, size)=>{
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if(keys.length > size){
+                cache.delete(keys[0]).then(limitCacheSize(name,size))
+            }
+        })
+    })
+}
 self.addEventListener('install', evt => {
     evt.waitUntil(
         caches.open(staticCacheName).then( cache => {
@@ -27,12 +39,10 @@ self.addEventListener('activate', evt => {
  evt.waitUntil(
      caches.keys().then( keys => {
          return Promise.all(
-             keys.filter( key =>  key !== staticCacheName)
+             keys.filter( key =>  key !== staticCacheName && key !== dynamicCacheName)
              .map( key =>  caches.delete(key) )
          )
-     }
-       
-     )
+     })
  )
 });
 
@@ -40,8 +50,17 @@ self.addEventListener('fetch', evt => {
   evt.respondWith( 
     caches.match(evt.request).then(
         cacheRes => {
-            return cacheRes || fetch(evt.request);
-        }
-    )
+            return cacheRes || fetch(evt.request).then(fetchResponse =>{
+              return caches.open(dynamicCacheName).then(cache =>{
+                  cache.put(evt.request.url,fetchResponse.clone());
+                  limitCacheSize(dynamicCacheName,1);
+                  return fetchResponse;
+              }); 
+            });
+        }).catch( () => {
+            if(evt.request.url.indexOf('.html') > -1) {
+                return caches.match('/pages/fallback.html'); 
+            }
+        })
   );
 })
